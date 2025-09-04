d54112c0a4ca02cd09bce9b53aca58d41ce8ce19
aldamosq <aldamosq@espol.edu.ec>
2025-09-04 00:06:02 -0500

    Actualizar prueba.js para incluir el cálculo de la suma y su resultado en la consola

--- prueba.js
+++ prueba.js
@@ -3,3 +3,8 @@
 console.log("Hola Mundo");
 console.log("Hola Mundo 2");
 console.log("Hola Mundo 3");
+
+let numero1 = 5;
+let numero2 = 10;
+let resultado = numero1 + numero2;
+console.log("El resultado de la suma es: " + resultado);
